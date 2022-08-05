e1bca677deeb6efd28d267707d86d0d006530e33
Daniel Reuther <d.reuther@hotmail.de>
2022-08-05 14:55:29 +0200

    first commit

---
+++ src/services/hero.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Hero } from '../components/heroes/hero';
+import { HEROES } from '../app/mock-data/mock-heroes';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HeroService {
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
+    return heroes;
+  }
+
+  constructor(private messageService: MessageService) {}
+}
